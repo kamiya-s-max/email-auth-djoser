9568b643f5eef4794addf64402e3b383ac332e5a
kamiya-s-max <s.kamiya.f15@gmail.com>
2021-04-18 17:24:30 +0900

    カスタムユーザーモデルの定義

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+import uuid
 
-# Create your models here.
+
+class User(AbstractUser):
+    """拡張ユーザーモデル"""
+    pass
+
